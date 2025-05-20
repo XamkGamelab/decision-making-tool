3a600b76bc4f1a6282c51350e8de0f31c0107279
Sydney Kolehmainen <fsyko001@edu.xamk.fi>
2025-05-20 14:59:48 +0300

    Pientä optimisaatiota StoryNode systeemeihin

--- Assets/Scripts/StoryNode.cs
+++ Assets/Scripts/StoryNode.cs
@@ -1,28 +1,47 @@
+using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewStoryNode", menuName = "ChooseYourAdventure/Story Node")]
+/// <summary>
+/// Represents a single node in the branching story system. Each node displays a  scenario, 
+/// a set of response options, and can be linked to other nodes for progression.
+/// </summary>
+[CreateAssetMenu(fileName = "NewStoryNode", menuName = "Scenarios/Story Node")]
 public class StoryNode : ScriptableObject
 {
-    [TextArea]
-    public string questionText;
+    [Header("Node Content")]
+    [Tooltip("The main text or question shown to the player for this story node.")]
+    [TextArea] public string questionText;
 
-    public string Button1Text = "1";
-    public string Button2Text = "2";
-    public string Button3Text = "3";
-    public string Button4Text = "4";
-
-    public StoryNode Node1;
-    public StoryNode Node2;
-    public StoryNode Node3;
-    public StoryNode Node4;
-
-    public bool showReturnToMenu;
-
-    public bool showButton1 = true;
-    public bool showButton2 = true;
-    public bool showButton3 = true;
-    public bool showButton4 = true;
-
+    [Tooltip("Optional image to visually support the node's content.")]
     public Sprite nodeImage;
-    public bool showImage;
+
+    [Tooltip("If true, the nodeImage will be shown in the UI. True by default.")]
+    public bool showImage = true;
+
+    [System.Serializable]
+    public class StoryOption
+    {
+        [Tooltip("The label that will be displayed on the choice button.")]
+        public string optionText = "Option";
+
+        [Tooltip("The next StoryNode to transition to when this option is selected.")]
+        public StoryNode nextNode;
+
+        [Tooltip("If false, this option will not be shown in the UI.")]
+        public bool isVisible = true;
+    }
+
+    [Tooltip("List of response options the player can choose from. Can be any length.")]
+    [SerializeField]
+    private List<StoryOption> options = new();
+
+    /// <summary>
+    /// Read-only access to the list of story options.
+    /// Use this instead of modifying the list directly to preserve integrity.
+    /// </summary>
+    public IReadOnlyList<StoryOption> Options => options;
+
+    [Header("Node Behavior")]
+    [Tooltip("If true, shows a button that returns the player to the main menu.")]
+    public bool showReturnToMenu;
 }
