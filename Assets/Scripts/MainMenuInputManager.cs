75240124b86a352fab44344ed840709c58894a1c
Sydney Kolehmainen <fsyko001@edu.xamk.fi>
2025-07-28 09:38:33 +0300

    Main menu korjauksia

--- Assets/Scripts/MainMenuInputManager.cs
+++ Assets/Scripts/MainMenuInputManager.cs
@@ -3,28 +3,10 @@ using UnityEngine;
 
 public class MainMenuInputManager : MonoBehaviour
 {
-    public TMP_InputField usernameInput;
-    public TMP_InputField yearInput;
 
-    private void Start()
+
+    public void CloseGame()
     {
-        // Lataa tallennetut arvot PlayerPrefsistä, jos niitä on
-        if (PlayerPrefs.HasKey("username"))
-            usernameInput.text = PlayerPrefs.GetString("username");
-
-        if (PlayerPrefs.HasKey("year"))
-            yearInput.text = PlayerPrefs.GetString("year");
-    }
-
-    public void OnUsernameChanged()
-    {
-        PlayerPrefs.SetString("username", usernameInput.text);
-        PlayerPrefs.Save();
-    }
-
-    public void OnYearChanged()
-    {
-        PlayerPrefs.SetString("year", yearInput.text);
-        PlayerPrefs.Save();
+        Application.Quit();
     }
 }
